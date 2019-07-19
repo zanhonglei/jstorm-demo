0c6856453e036be0031e848d0ec5adc6aa747139
昝红磊 <honglei_zan@jit.com.cn>
2019-07-19 14:19:31 +0800

    补全bolt和spout 测试分组

--- src/main/java/com/xy/topology/spout/FetchUserSpout.java
+++ src/main/java/com/xy/topology/spout/FetchUserSpout.java
@@ -6,10 +6,8 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
-import com.alibaba.fastjson.JSON;
 import com.xy.topology.model.UserInfo;
 import com.xy.topology.utils.Utils;
-import shade.storm.org.json.simple.JSONValue;
 
 import java.util.Date;
 import java.util.Map;
